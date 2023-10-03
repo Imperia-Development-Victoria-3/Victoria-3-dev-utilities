f44973f5ca0bed238460fd833e1dd06f6717282d
HiddeLekanne <hiddelekanne@gmail.com>
2023-10-03 12:04:45 +0200

    normalize button fix

--- apps/app_buy_packages.py
+++ apps/app_buy_packages.py
@@ -17,7 +17,7 @@ def get_layout():
         html.Div(id="hidden-output", style={"display": "none"}),
         dcc.Store(id='table-selected-prev'),
         html.Button('Save to mod folder', id='save-button', n_clicks=0),
-        html.Button('Normalize', id='normalize-button', n_clicks=0),
+        html.Button('Normalize', id='normalize-button', hidden=True, n_clicks=0),
         dcc.Checklist(
             options={
                 'price': 'Apply Price Adjustment',
