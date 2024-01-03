08e94f6e129205767795aac25bb8b717d408cdbb
HiddeLekanne <hiddelekanne@gmail.com>
2024-01-04 00:16:48 +0100

    it is now a version where newlines are not ignored, plus a reformat
    Is now less aggressive towards optimization and is reformatted to handle future style objects to adjust the style per file, or keyword.

--- parser/test.py
+++ parser/test.py
@@ -1,56 +1,50 @@
 import warnings
 from ply import yacc, lex
-
-### TODO: Something about color schemes; rgb{0 0 0} and hsv{0 0 0}
+from enum import IntEnum, auto
 
 # --- Tokenizer
 
 # All tokens must be named in advance.
-# tokens = ('BASIC_WORD', 'DIVIDER', 'NOT_EQUAL', 'BEGIN_DICT', 'END_DICT', 'GREATER', 'EQUAL_OR_GREATER',
-#           'EQUAL_OR_LESSER', 'LESSER', 'EQUAL_AND_EXISTS', 'COMMENT')
-
-tokens = ('BASIC_WORD', 'DIVIDER', 'BEGIN_DICT', 'END_DICT', "FULL_COMMENT", "PART_COMMENT")
+tokens = ('BASIC_WORD', 'DIVIDER', 'BEGIN_DICT', 'END_DICT', "COMMENT", "NEW_LINE")
 
 # Ignored characters
 t_ignore = ' \t'
 
 # Token matching rules are written as regexs
-t_BASIC_WORD = r'([\'\[\];\*\@\!\$:\w\.\/\"\|(\(\)-]+)|' \
-               r'(\"[^\n]+\")'
-t_DIVIDER = r'[<>\?=]+'
+t_BASIC_WORD = r'([\@\'\[\];\*\$:\w\.\/\"\|\(\)-]+)'
+t_DIVIDER = r'[!<>\?=]+'
 t_BEGIN_DICT = r'\{'
 t_END_DICT = r'\}'
 
 
-# ORDER OF THE TWO COMMENTS MATTER, the more general should go first in order to be overwritten by the subset.
-# WE CAN'T DO A SPECIAL CASE BECAUSE THE INFORMATION REQUIRED TO DETERMINE THAT ISN'T AVAILABLE IN THE GENERAL CASE.
-def t_FULL_COMMENT(t):
-    r"(\n|^)\s*\#.*"
-    t.value = re.search(r"#.*", t.value).group(0)  # get rid of newline and whitespaces before comment
-    return t
-
-
-def t_PART_COMMENT(t):
+def t_COMMENT(t):
     r"""\#.*"""
-    # all the comments to be filtered by full comments
     return t
 
 
 def t_special_word(t):
-    r'(\@\[.*?\])'
+    r"""(\@\[.*?\])"""
+    t.type = "BASIC_WORD"
+    return t
+
+
+def t_special_word_2(t):
+    r"""("[^\n]+")"""
     t.type = "BASIC_WORD"
     return t
 
 
 def t_color_word(t):
-    r'((rgb)|(hsv)|(hsv360))\s*\{\s*\d+(\.\d+)*\s+\d+(\.\d+)*\s+\d+(\.\d+)*\s*\}'
+    r"""((rgb)|((hsv)|((hsv360)))|(hsv360))\s*\{\s*\d+(\.\d+)*\s+\d+(\.\d+)*\s+\d+(\.\d+)*\s*\}"""
     t.type = "BASIC_WORD"
     return t
 
 
-def t_ignore_newline(t):
-    r'\n+'
-    t.lexer.lineno += t.value.count('\n')
+def t_newline(t):
+    r"""\n"""
+    t.lexer.lineno += 1
+    t.type = "NEW_LINE"
+    return t
 
 
 # A function can be used if there is an associated action.
@@ -65,75 +59,89 @@ def t_error(t):
 lexer = lex.lex()
 
 
+class ParseTypes(IntEnum):
+    LIST = auto()
+    ASSIGNMENT = auto()
+    ELEMENT = auto()
+    FULL_LINE_COMMENT = auto()
+    COMMENT = auto()
+    OBJECT = auto()
+    BEGIN = auto()
+    BEGIN_COMMENT = auto()
+    END = auto()
+    END_COMMENT = auto()
+    DOUBLE_NEWLINE = auto()
+    NEW_LINE = auto()
+
+
 # --- Parser
-
-# Write functions for each grammar rule which is
-# specified in the docstring.
-# def p_comment_accumulator(p):
-#     """
-#     full_comment : FULL_COMMENT
-#                  | FULL_COMMENT full_comment
-#     """
-#     p[0] = [p[1]] if len(p) == 2 else p[2] + [p[1]]
-
-
 def p_program(p):
     """
     program : assignment
             | program assignment
     """
-    p[0] = [p[1]] if len(p) == 2 else p[1] + [p[2]]
+    if len(p) == 2:
+        p[0] = (ParseTypes.LIST, [p[1]])
+    else:
+        p[0] = (ParseTypes.LIST, p[1][1] + [p[2]])
 
 
 def p_assignment(p):
     """
     assignment : BASIC_WORD DIVIDER value
     """
-    p[0] = ((p[2], p[1]), p[3])
+    p[0] = (ParseTypes.ASSIGNMENT, p[2], p[1], p[3])
 
 
 def p_assignment_identifier(p):
-    'assignment : BASIC_WORD'
-    p[0] = (("=", p[1]), True)  # Or a default value instead of None
+    """assignment : BASIC_WORD"""
+    p[0] = (ParseTypes.ELEMENT, p[1])  # Or a default value instead of None
 
 
 def p_standalone_comment(p):
-    'assignment : FULL_COMMENT'
-    p[0] = (('C', p[1]), True)  # Handling standalone comments
+    """assignment : NEW_LINE COMMENT
+                  | double_newline COMMENT
+                  | COMMENT"""
+    if len(p) > 2:
+        if isinstance(p[1], tuple):
+            p[1] = p[1][1]
+        p[0] = (ParseTypes.FULL_LINE_COMMENT, p[1] + p[2])  # Handling standalone comments
+    else:
+        p[0] = (ParseTypes.FULL_LINE_COMMENT, p[1])
 
 
 def p_attached_comment_assignment(p):
-    """assignment : assignment PART_COMMENT """
-    p[0] = (p[1][0], (("P", p[1][1]), p[2]))  # Or a default value instead of None
+    """assignment : assignment COMMENT """
+    p[0] = (ParseTypes.COMMENT, p[1], p[2])  # Or a default value instead of None
 
 
 def p_attached_comment_begin(p):
     """
-    begin_dict : BEGIN_DICT PART_COMMENT
+    begin_dict : BEGIN_DICT COMMENT
                | BEGIN_DICT
     """
-    if len(p) == 2:
-        p[0] = p[1]
+    if len(p) == 3:
+        p[0] = (ParseTypes.BEGIN_COMMENT, p[1], p[2])
     else:
-        p[0] = (("P", p[1]), p[2])
+        p[0] = (ParseTypes.BEGIN, p[1])
 
 
 def p_attached_comment_end(p):
     """
-    end_dict : END_DICT PART_COMMENT
+    end_dict : END_DICT COMMENT
              | END_DICT
     """
-    if len(p) == 2:
-        p[0] = p[1]
+    if len(p) == 3:
+        p[0] = (ParseTypes.END_COMMENT, p[1], p[2])
     else:
-        p[0] = (("P", p[1]), p[2])
+        p[0] = (ParseTypes.END, p[1])
 
 
 def p_promote_2(p):
     """
     value : BASIC_WORD
     """
-    p[0] = p[1]
+    p[0] = (ParseTypes.ELEMENT, p[1])
 
 
 def p_promote(p):
@@ -149,9 +157,9 @@ def p_map(p):
            | begin_dict end_dict
     """
     if len(p) == 4:
-        p[0] = (p[1], p[2], p[3])
+        p[0] = (ParseTypes.OBJECT, p[1], p[2], p[3])
     else:
-        p[0] = (p[1], [], p[2])
+        p[0] = (ParseTypes.OBJECT, p[1], [], p[2])
 
 
 def p_assignments_single(p):
@@ -160,9 +168,44 @@ def p_assignments_single(p):
                 | assignments assignment
     """
     if len(p) == 2:
-        p[0] = [p[1]]
+        p[0] = (ParseTypes.LIST, [p[1]])
     else:
-        p[0] = p[1] + [p[2]]
+        p[0] = (ParseTypes.LIST, p[1][1] + [p[2]])
+
+
+def p_assignments_object(p):
+    """
+    assignments : object
+                | assignments object
+    """
+    if len(p) == 2:
+        p[0] = (ParseTypes.LIST, [p[1]])
+    else:
+        p[0] = (ParseTypes.LIST, p[1][1] + [p[2]])
+
+
+def p_double_new_line(p):
+    """
+    double_newline : NEW_LINE NEW_LINE
+                   | double_newline NEW_LINE
+    """
+    if isinstance(p[1], tuple):
+        p[1] = p[1][1]
+    p[0] = (ParseTypes.DOUBLE_NEWLINE, p[1] + p[2])
+
+
+def p_double_new_line_promote(p):
+    """
+    assignment : double_newline
+    """
+    p[0] = p[1]
+
+
+def p_new_line(p):
+    """
+    assignment : NEW_LINE
+    """
+    p[0] = (ParseTypes.NEW_LINE, p[1])
 
 
 def p_error(p):
@@ -183,48 +226,68 @@ parser = yacc.yacc()
 
 
 def reconstruct(parsed_data):
-    def reconstruct_element(element, level=1):
-        if isinstance(element, tuple):
-            relationship, key = element[0][0], element[0][1]
-            if relationship == 'C':  # Handle comments
-                return key
-            elif relationship == 'P':  # Handle post-item comments
-                comment = element[1]
-                if isinstance(key, bool):
-                    return f'\t{comment}'
-                return f'{key}\t{comment}'
-            elif re.match(t_DIVIDER, relationship):  # Handle key-value pairs
-                value = element[1]
-                if isinstance(value, tuple) and len(value) == 3:
-                    start_symbol, end_symbol = value[0], value[2]
-                    value = value[1]
+    def reconstruct_element(element, level=0, tabs_added=False):
+        if not tabs_added:
+            tabs = "\t" * level
+        else:
+            tabs = ""
 
-                if isinstance(value, list):  # If value is a list, recursively process it
-                    value = reconstruct_element(start_symbol, level + 1) + '\n' + '\n'.join(
-                        [level * '\t' + reconstruct_element(v, level + 1) for v in value]) + '\n' + (
-                                    level - 1) * '\t' + reconstruct_element(end_symbol, level + 1)
-                elif isinstance(value, bool):
-                    return f'{key}'
-                elif isinstance(value, tuple):
-                    if isinstance(value[0][1], bool):
-                        return f'{key}{reconstruct_element(value)}'
-                    return f'{key} {relationship} {reconstruct_element(value)}'
-                return f'{key} {relationship} {value}'
-        elif isinstance(element, str):  # Handle simple strings
-            return element
-        return ''
+        if isinstance(element, tuple):
+            parse_type = element[0]
+            if parse_type == ParseTypes.LIST:
+                if tabs_added:
+                    string = " "
+                else:
+                    string = ""
+                for item in element[1]:
+                    if tabs_added:
+                        string += f"{reconstruct_element(item, level=level, tabs_added=tabs_added)} "
+                    else:
+                        string += f"{reconstruct_element(item, level=level, tabs_added=tabs_added)}"
+                return string
+            elif parse_type == ParseTypes.ASSIGNMENT:
+                return f"{tabs}{element[2]} {element[1]} {reconstruct_element(element[3], level=level, tabs_added=True)}"
+            elif parse_type == ParseTypes.ELEMENT:
+                return f"{tabs}{element[1]}"
+            elif parse_type == ParseTypes.FULL_LINE_COMMENT:
+                tmp = element[1].split('\n')
+                tmp[-1] = tabs + tmp[-1]
+                return "\n".join(tmp)
+            elif parse_type == ParseTypes.COMMENT:
+                return f"{reconstruct_element(element[1], level=level, tabs_added=tabs_added)}\t{element[2]}"
+            elif parse_type == ParseTypes.OBJECT:
+                # if config["single_element_single_line"]:
+                if not element[2]:
+                    return f"{reconstruct_element(element[1], level=level, tabs_added=True)}{reconstruct_element(element[3], level=level, tabs_added=True)}"
+                else:
+                    if not any(item[0] == ParseTypes.NEW_LINE or item[0] == ParseTypes.DOUBLE_NEWLINE for item in
+                               element[2][1]):
+                        tabs_added = True
+                    else:
+                        tabs_added = False
+                    return f"{reconstruct_element(element[1], level=level)}{reconstruct_element(element[2], level=level + 1, tabs_added=tabs_added)}{reconstruct_element(element[3], level=level, tabs_added=tabs_added)}"
+            elif parse_type == ParseTypes.BEGIN:
+                return f"{element[1]}"
+            elif parse_type == ParseTypes.END:
+                return f"{tabs}{element[1]}"
+            elif parse_type == ParseTypes.BEGIN_COMMENT:
+                return f"{element[1]}\t{element[2]}"
+            elif parse_type == ParseTypes.END_COMMENT:
+                return f"{tabs}{element[1]}\t{element[2]}"
+            elif parse_type == ParseTypes.DOUBLE_NEWLINE:
+                return f"\n{tabs}\n"
+            elif parse_type == ParseTypes.NEW_LINE:
+                return f"\n"
+        return ""
 
     # Start processing from the outermost layer
-    return '\n'.join([reconstruct_element(item) for item in parsed_data]) + '\n'
+    return reconstruct_element(parsed_data)
 
 
 context_size = 100
 
 
-def test_text_reconstruction(parser_func, reconstruct_func, original_text):
-    parsed_data = parser_func.parse(original_text)
-    reconstructed_text = reconstruct_func(parsed_data)
-
+def test_text_reconstruction(original_text, reconstructed_text):
     # Remove all whitespaces for comparison
     original_text_no_whitespace = ''.join(original_text.split())
     reconstructed_text_no_whitespace = ''.join(reconstructed_text.split())
@@ -251,50 +314,88 @@ def test_text_reconstruction(parser_func, reconstruct_func, original_text):
     return True
 
 
-def test_parsing_consistency(parser_func, reconstruct_func, original_text):
-    original_parsed_data = parser_func.parse(original_text)
-    reconstructed_text = reconstruct_func(original_parsed_data)
-    reconstructed_parsed_data = parser_func.parse(reconstructed_text)
+def test_parsing_consistency(original_parsed_data, reconstructed_parsed_data):
     return original_parsed_data == reconstructed_parsed_data
 
 
-test_input = """#something else
-
-### asoerlae ###
-## something else ##
-building_university = {
-
-	building_group = bg_technology
-
-	city_type = city
-
-	levels_per_mesh = 5
-
-	unlocking_technologies = { 
-		academia
-	}
-    # random comment
-	production_method_groups = { 
-		pmg_base_building_university = { hoela } # something
-		pmg_base_building_university = peela # this goes right
-		pmg_university_academia
-	} 
+test_input = """
+decree_encourage_urban_services = {
+	texture = "gfx/interface/icons/decree/urbans.dds"
+	unlocking_technologies = { # somerhing
+		urban_planning
+	} # adesfjaef
 	
-	something # this goes wrong?
-    this_does_not
-	texture = "gfx/interface/icons/building_icons/building_university.dds"
-
-	required_construction = construction_cost_medium
-}
-"""
+	#	valid = {
+	#		NOR = { 
+	#			has_decree = decree_encourage_manufacturing_industry 
+	#			has_decree = decree_encourage_agricultural_industry
+	
+	#			has_decree = decree_encourage_resource_industry
+	#		}
+	#	}
+		
+	modifier = {
+		# building_group_bg_trade_throughput_add = 0.2 (non-functional)
+		# Need to add urban center output, unsure how to go about it
+		building_group_bg_service_throughput_add = 0.2
+		building_port_throughput_add = 0.2
+		building_railway_throughput_add = 0.2
+		building_group_bg_monuments_throughput_add = 0.2
+		building_group_bg_canals_throughput_add = 0.2
+		building_group_bg_power_throughput_add = 0.2
+	}
+	cost = 35 # was 75
+	ai_weight = {
+		value = 0
+		if = {
+			limit = {
+				any_scope_building = {
+					is_building_group = bg_urban_facilities
+				}
+			}
+			add = 50
+		}
+		if = {
+			limit = {
+				any_scope_building = {
+					is_building_group = bg_urban_facilities
+					count >= 3
+				}
+			}
+			add = 50
+		}
+		if = {
+			limit = {
+				any_scope_building = {
+					is_building_group = bg_urban_facilities
+					count >= 5
+				}
+			}
+			add = 50
+		}
+		if = {
+			limit = {
+				scope:country = {
+					has_strategy = ai_strategy_placate_population
+				}
+			}
+			multiply = 1.5
+		}
+	}
+}"""
 
 
 def test_file(file_path, parser_func, reconstruct_func):
     # Read the content
     with open(file_path, 'r', encoding='utf-8-sig') as file:
         content = file.read()
-    return test_text_reconstruction(parser, reconstruct, content), test_parsing_consistency(parser_func,
-                                                                                            reconstruct_func, content)
+
+    parsed_data = parser_func.parse(content)
+    reconstructed_text = reconstruct_func(parsed_data)
+    reconstructed_parsed_data = parser_func.parse(reconstructed_text)
+
+    return test_text_reconstruction(content, reconstructed_text), test_parsing_consistency(parsed_data,
+                                                                                           reconstructed_parsed_data)
 
 
 blacklist = {
@@ -311,11 +412,11 @@ def test_all_txt_files(folder_path, parser_func, reconstruct_func):
     for root, dirs, files in os.walk(folder_path):
         for file_name in files:
             if file_name.endswith('.txt'):
-                total_files += 1
                 file_path = os.path.join(root, file_name)
-                if os.path.getsize(file_path) == 0:
-                    print("\n\n it works \n\n\n\n\n")
+                if os.path.getsize(file_path) <= 3:
                     continue
+
+                total_files += 1
 
                 print("Testing", file_path)
                 try:
@@ -330,7 +431,7 @@ def test_all_txt_files(folder_path, parser_func, reconstruct_func):
                     passed_consistency += 1
 
                 # Track failed files
-                if not (reconstruction_bool and consistency_bool):
+                if not reconstruction_bool or not consistency_bool:
                     failed_files.append(file_path)
 
     # Calculate percentages
@@ -346,6 +447,7 @@ def test_all_txt_files(folder_path, parser_func, reconstruct_func):
 
     return report
 
+
 def process_file(file_path, parser_func, reconstruct_func):
     # Read the content
     with open(file_path, 'r', encoding='utf-8-sig') as file:
@@ -353,7 +455,7 @@ def process_file(file_path, parser_func, reconstruct_func):
 
     original_parsed_data = parser_func.parse(content)
     reconstructed_text = reconstruct_func(original_parsed_data)
-
+    #
     with open(file_path, 'w', encoding='utf-8-sig') as file:
         file.write(reconstructed_text)
 
@@ -367,36 +469,37 @@ def process_all_txt_files(folder_path, parser_func, reconstruct_func):
                 decoded_content = process_file(file_path, parser_func, reconstruct_func)
 
 
-
 if __name__ == '__main__':
     from constants import Test
     import os
     import re
+    import pprint
 
     # print(re.search(t_PART_COMMENT,test_input ))
     from constants import Test
     import os
 
-    process_all_txt_files(Test.mod_directory, parser, reconstruct)
+    # path = os.path.join(Test.game_directory, "common\\")
+    # path = os.path.normpath(
+    #     'C:\\Program Files (x86)\\Steam\\steamapps\\common\\Victoria 3\\game\\common\\coat_of_arms\\coat_of_arms\\02_countries.txt')
 
-    # pprint.pprint(blacklist)
-    # path = os.path.join(Test.game_directory,
-    #                     os.path.normpath("common/"))
-    # print(test_all_txt_files(path, parser, reconstruct))
-    # print(os.path.getsize(blacklist[2]))
-    # path = blacklist[8]
-    # # path = os.path.normpath("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Victoria 3\\game\\common\\on_actions\\00_code_on_actions.txt")
-    # # path = os.path.join(Test.game_directory, "common\\buildings\\03_mines.txt")
+    process_all_txt_files(Test.mod_directory, parser, reconstruct)
+    # print(test_all_txt_files(Test.mod_directory, parser, reconstruct))
+
     # with open(path, encoding='utf-8-sig') as file:
     #     file_string = file.read()
-    # print(path)
-    # print(test_text_reconstruction(parser, reconstruct, file_string))
-    # print(test_parsing_consistency(parser, reconstruct, file_string))
+    # # print(path)
+
+    # parsed_data = parser.parse(test_input)
+    # reconstructed_text = reconstruct(parsed_data)
+    # reconstructed_parsed_data = parser.parse(reconstructed_text)
     #
+    # print(test_text_reconstruction(test_input, reconstructed_text))
+    # print(test_parsing_consistency(parsed_data, reconstructed_parsed_data))
+    # #
     # # print(file_string)
     # # Parse an expression
-    # ast = parser.parse(file_string)
-    # # pprint.pprint(ast)
-    # if ast:
-    #     thing = reconstruct(ast)
-    #     # print(thing)
+    # # pprint.pprint(reconstructed_parsed_data)
+    # if parsed_data:
+    #     thing = reconstruct(reconstructed_parsed_data)
+    #     print(thing)
